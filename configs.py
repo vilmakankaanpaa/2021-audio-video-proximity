96acfd1a2ba27795cf06b2fb6bf8775b9846bf45
Kankaanpää Vilma <vilma.kankaanpaa@aalto.fi>
2020-06-11 12:48:39 +0300

    Configs of G sheets file

--- configs.py
+++ configs.py
@@ -3,3 +3,6 @@ VIDEO_PATH = "test_video_1.mp4"
 USE_AUDIO = False # separate audio file
 USE_VIDEO = True
 VIDEO_AUDIO_ON = False
+G_LOG_FILE = "Monkey-logs"
+G_LOG_SHEET_MAIN = "main-logs"
+G_LOG_SHEET_ALIVE = "alive-logs"
