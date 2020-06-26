d5384a3b6b5dfa93d2a5ecb7697804a5750035ce
Vilma Kankaanpää <vilma.kankaanpaa@aalto.fi>
2020-06-26 14:43:35 +0300

    changes due to new directory name

--- launcher.sh
+++ launcher.sh
@@ -5,6 +5,6 @@
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games
 
 cd /
-cd home/pi/monkeys
+cd home/pi/sakis-video-tunnel
 python3 ir_distance.py
 cd /
