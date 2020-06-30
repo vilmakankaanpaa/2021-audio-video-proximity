13f369898a10e3d61f5533bef1af21298cbfb945
Kankaanpää Vilma <vilma.kankaanpaa@aalto.fi>
2020-06-30 09:58:55 +0300

    sensor functions moved to sensor.py

--- monkeytunnel.py
+++ monkeytunnel.py
@@ -2,8 +2,8 @@ import os
 import sys
 from time import sleep
 from datetime import datetime, date, time
-import spidev
 
+from sensors import check_sensors 
 from audioplayer import AudioPlayer
 from videoplayer import VideoPlayer
 from logger import Logger
@@ -11,49 +11,6 @@ from camera import Camera
 import configs
 
 sys.excepthook = sys.__excepthook__
-
-# Spidev used to connect to and read the sensors
-spi = spidev.SpiDev()
-spi.open(0,0)
-
-def read_channel(channel):
-  val = spi.xfer2([1,(8+channel)<<4,0])
-  data = ((val[1]&3) << 8) + val[2]
-  return data
-
-def get_volts(channel=0):
-    volts = (read_channel(channel)/1023.0)*3.3
-    return volts
-
-def is_in_range(voltsValue, sensorIndex):
-    # Threshold for every sensor: probably depends on the location
-    # and have to be tested and adjusted
-
-    if sensorIndex == 0 and voltsValue > 0.50:
-        # rightmost sensor when looking "at the screen"
-        return True
-    elif sensorIndex == 1 and voltsValue > 0.80:
-        # left of above
-        return True
-    elif sensorIndex == 2 and voltsValue > 0.30:
-        # left of above
-        return True
-    else:
-        return False
-
-def check_sensors():
-
-    i = 0
-    sensorVolts = []
-    sensorResults = []
-    for i in range(3):
-        v = get_volts(i)
-        sensorVolts.append(v)
-        r = is_in_range(v, i)
-        sensorResults.append(r)
-
-    return sensorVolts, sensorResults
-
 
 def new_video_name(logger):
     """have to first create new folder per date, if using"""
@@ -65,7 +22,6 @@ def new_video_name(logger):
     ixID, ixStart = logger.get_ix_info()
     name = ixID + '_' + ixStart
     return name
-
 
 def print_configurations():
 
---
+++ sensors.py
@@ -0,0 +1,47 @@
+import spidev
+
+# Spidev used to connect to and read the sensors
+spi = spidev.SpiDev()
+spi.open(0,0)
+
+
+def read_channel(channel):
+  val = spi.xfer2([1,(8+channel)<<4,0])
+  data = ((val[1]&3) << 8) + val[2]
+  return data
+
+
+def get_volts(channel=0):
+    volts = (read_channel(channel)/1023.0)*3.3
+    return volts
+
+
+def is_in_range(voltsValue, sensorIndex):
+    # Threshold for every sensor: probably depends on the location
+    # and have to be tested and adjusted
+
+    if sensorIndex == 0 and voltsValue > 0.50:
+        # rightmost sensor when looking "at the screen"
+        return True
+    elif sensorIndex == 1 and voltsValue > 0.80:
+        # left of above
+        return True
+    elif sensorIndex == 2 and voltsValue > 0.30:
+        # left of above
+        return True
+    else:
+        return False
+
+
+def check_sensors():
+
+    i = 0
+    sensorVolts = []
+    sensorResults = []
+    for i in range(3):
+        v = get_volts(i)
+        sensorVolts.append(v)
+        r = is_in_range(v, i)
+        sensorResults.append(r)
+
+    return sensorVolts, sensorResults
