c53afafcca64a2b9fce1b5f6c0ee7b48613f9216
Vilma Kankaanpää <vilma.kankaanpaa@aalto.fi>
2020-07-03 19:48:02 +0300

    tried manual mounting bc might help with usb permissions

--- launcher.sh
+++ launcher.sh
@@ -3,6 +3,10 @@
 
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games
 
+# mount usb stick here - automatic mounting did not ...
+# ...work for getting permissions for python
+sudo mount /dev/sda1 /home/pi/sakis-video-tunnel/USB
+
 # kill omxplayer from previous run if on!
 killall "omxplayer.bin"
 pkill -9 python3
