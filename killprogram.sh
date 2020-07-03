8bf0cb7da10f236790cdaf22cd5561df1f838f10
Vilma Kankaanpää <vilma.kankaanpaa@aalto.fi>
2020-07-03 16:59:32 +0300

    some changes to shell files

--- killprogram.sh
+++ killprogram.sh
@@ -3,12 +3,3 @@
 pkill -9 python3
 pkill -9 launcher.sh
 killall "omxplayer.bin"
-
-# in python:
-#import os
-
-#def kill_running_programs():
-
-#  os.system('pkill -9 python3')
-#  os.system('pkill -9 launcher.sh')
-#  os.system('killall \"omxplayer.bin\"')
--- launcher.sh
+++ launcher.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 # launcher.sh
-# go to home, then here, then execute python script, then back home
 
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games
 
+# kill omxplayer from previous run if on!
+killall "omxplayer.bin"
+pkill -9 python3
+
 cd /
 cd home/pi/sakis-video-tunnel
-python3 monkeytunnel.py >> output.txt
+python3 monkeytunnel.py 
+# >> output.txt
 cd /
+
