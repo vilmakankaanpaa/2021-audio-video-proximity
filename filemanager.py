f5bcb28b0f2c2c9bccf2f3d60ae525d9634b7efd
Vilma Kankaanpää <vilma.kankaanpaa@aalto.fi>
2020-07-10 13:42:27 +0300

    bug fixes and updates on printing

--- filemanager.py
+++ filemanager.py
@@ -9,17 +9,17 @@ import configs
 
 sys.excepthook = sys.__excepthook__
 
-def printlog(msg, srcfile):
+def printlog(srcfile, msg):
 
     print(msg)
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     data = [[timestamp, srcfile, msg]]
 
-    self.log_local(data=data, sheet=configs.local_printlog)
+    log_local(data=data, sheet=configs.local_printlog)
 
 
 def log_local(data, sheet):
-    print(datetime.isoformat(datetime.now()),'Logging to local file:',sheet)
+    
     with open(sheet, 'a', newline='') as logfile:
         logwriter = csv.writer(logfile, delimiter=',')
         for row in data:
@@ -30,20 +30,16 @@ def delete_local_file(path):
 
     if os.path.exists(path):
         os.remove(path)
-        print('Removed file at', path)
     else:
-        print('Could not delete local file at {}, it does not exist'.format(path))
+        printlog('Filemanager','Could not delete local file at {}, it does not exist'.format(path))
 
 
 def list_recordings():
     videos = []
     pathdir = None
 
-    print('Listing recordings..')
-
     try:
         dirContent = os.listdir(configs.RECORDINGS_PATH)
-        print('Size of dir:', len(dirContent))
         if len(dirContent) == 0:
             # there is nothing in the folder while there should be at least test.txt
             # --> could not read directory
@@ -54,14 +50,12 @@ def list_recordings():
         pathdir = configs.RECORDINGS_PATH
 
     except:
+        printlog('Filemanager','ERROR – Setting recordings folder to Pi local!')
         dirContent = os.listdir(configs.RECORDINGS_PATH_2)
         for filename in dirContent:
             if filename.endswith('.h264'):
                 videos.append(filename)
         pathdir = configs.RECORDINGS_PATH_2
-
-    print(datetime.isoformat(datetime.now()), 'Path for videos:', pathdir)
-    print(datetime.isoformat(datetime.now()),'Videos:', videos)
 
     return videos, pathdir
 
@@ -69,15 +63,21 @@ def list_recordings():
 def nof_recordings():
 
     try:
+        
         dirContent = os.listdir(configs.RECORDINGS_PATH)
         if len(dirContent) == 0:
             raise
         else:
             # there is one file for testing in the folder: test.txt, hence -1
+            printlog('Filemanager','No. of recordings in {} is {}.'.format(
+                configs.RECORDINGS_PATH, (len(dirContent)-1)))
             return len(dirContent)-1
     except:
         dirContent = os.listdir(configs.RECORDINGS_PATH_2)
 
+    printlog('Filemanager','No. of recordings in {} is {}.'.format(
+                configs.RECORDINGS_PATH_2, len(dirContent)))
+    
     return len(dirContent)
 
 
