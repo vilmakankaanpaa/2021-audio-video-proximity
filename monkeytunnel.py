d30655412ac1d2f61157607ca7fa2d5b7486a4a9
Vilma Kankaanpää <vilma.kankaanpaa@aalto.fi>
2020-07-20 10:31:15 +0300

    10 loops takes 4 seconds. so factor 5 for threshold seems fine for now

--- monkeytunnel.py
+++ monkeytunnel.py
@@ -132,15 +132,15 @@ if __name__ == "__main__":
     logger = Logger(pid)
     logger.log_program_run_info()
     
-    k = 0
+#    k = 0
 
     while True:
 
-        if (k == 10):
-            print(datetime.isoformat(datetime.now()),'ping!')
-            k=0
-        else:
-            k += 1
+#        if (k == 10):
+#            print(datetime.isoformat(datetime.now()),'ping!')
+#            k=0
+#        else:
+#            k += 1
             
         # Checking if should update the request quota for Google Sheets
         # It is 100 requests per 100 seconds (e.g. logging of 100 rows)
