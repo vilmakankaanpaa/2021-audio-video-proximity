34bc80817da017aaf072201078be790f06255fb8
Vilma Kankaanpää <vilma.kankaanpaa@aalto.fi>
2021-03-29 11:47:01 +0300

    updated .launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -9,15 +9,11 @@
 # Short-Description: Simple script to start a program at boot
 ### END INIT INFO
 
-# mount usb stick here - automatic mounting did not ...
-# ...work for getting permissions for python program
-sudo mount /dev/sda1 /mnt/kingston
-
 # move the mouse to different location, because at the start it would be on
 # the left corner and would trigger the task bar and leave it there
 xdotool mousemove 100 100
 
 # start the monkeytunnel program
-cd /home/pi/sakis-video-tunnel
-sudo python3 monkeytunnel.py >> output.txt
+cd /home/pi/sakis-tunnel-2021
+sudo python monkeytunnel.py >> output.txt
 cd /
