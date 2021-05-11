f0a9fe0bed457dce3588a043d0895afd5b13c604
Vilma Kankaanpää <vilma.kankaanpaa@aalto.fi>
2021-05-11 14:23:54 +0300

    simplifying

--- ir_sensors.py
+++ ir_sensors.py
@@ -9,31 +9,25 @@ spi.open(0,0)
 spi.max_speed_hz=1000000
 
 
-def read_channel(channel):
-  
+def read_channel(channel=0):
+
 
   val = spi.xfer2([1,(8+channel)<<4,0])
   data = ((val[1]&3) << 8) + val[2]
-  return data
-
-
-def get_volts(channel=0):
-    volts = (read_channel(channel)/1023.0)*3.3
-    return volts
+  volts = (read_channel(channel)/1023.0)*3.3
+  return volts
 
 
 def is_in_range(voltsValue, sensorIndex):
     # Threshold for every sensor: probably depends on the location
     # and have to be tested and adjusted
 
+    # 0-3 from right to left when facing screen
     if sensorIndex == 0 and voltsValue > 0.40:
-        # rightmost sensor when looking "at the screen"
         return True
     elif sensorIndex == 1 and voltsValue > 0.50:
-        # left of above
         return True
     elif sensorIndex == 2 and voltsValue > 0.50:
-        # left of above
         return True
     else:
         return False
@@ -41,10 +35,10 @@ def is_in_range(voltsValue, sensorIndex):
 
 def check_sensors():
 
-    sensorVolts = []
+    sensorVolts = [] # the closer, the larger volts
     sensorResults = []
     for i in range(3):
-        v = get_volts(i)
+        v = read_channel(i)
         sensorVolts.append(v)
         r = is_in_range(v, i)
         sensorResults.append(r)
