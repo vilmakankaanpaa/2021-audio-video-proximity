593ac6042402e2fe92dd1514e1f24bb7b813d04e
Vilma Kankaanpää <vilma.kankaanpaa@aalto.fi>
2021-05-12 18:34:14 +0300

    Fixing microphone trying to record to wrong dir.

--- microphone.py
+++ microphone.py
@@ -1,4 +1,5 @@
 import subprocess
+import configs
 
 class Microphone():
 
@@ -9,7 +10,7 @@ class Microphone():
     def record(self, filename):
 
         if not self.is_recording:
-            filepath = '/home/pi/sakis-tunnel-2021/mic-records/' + filename + '.wav'
+            filepath = configs.MIC_RECORDINGS + filename + '.wav'
             self.recorder = subprocess.Popen(args=["arecord", "--format=S16_LE", "--file-type=wav", filepath], stdout=subprocess.PIPE,stderr=subprocess.PIPE,text=True)
             self.is_recording = True
 
