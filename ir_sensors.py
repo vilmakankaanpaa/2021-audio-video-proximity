050470b5f3dd024531f309a22e20053840445767
Vilma Kankaanpää <vilma.kankaanpaa@aalto.fi>
2021-05-12 16:52:16 +0300

    Cleaning old code from ir_sensors.

--- ir_sensors.py
+++ ir_sensors.py
@@ -12,18 +12,16 @@ class Sensors():
 
     def __init__(self):
 
-        # Channels and thresholds. 0-3 from right to left when facing screen. Might
-        # depend on location so good to test every time the device moves.
-        self.voltThresholds = {0:0.40, 1:0.50, 2:0.50}
-        self.checkThreshold = 3
-        self.monkeyInside = False
+        # Channels and thresholds. 0-3 from right to left when facing screen.
+        # Might depend on location so good to test every time the device moves.
+        self.voltThresholds = {0:0.34, 1:0.30, 2:0.16}
+
         self.sensorReadings = [[False,0],[False,0],[False,0]]
 
+        # After how many checks the value is changed and "determined".
+        self.checkThreshold = 3
         # Determined state of sensors
         self.activatedSensors = [False, False, False]
-
-        # # Save the first timestamp when sensor gave FALSE reading
-        # self.firstFalseReadingTime = None
 
 
     def read_channel(self, channel=0):
@@ -36,10 +34,11 @@ class Sensors():
 
     def single_sensor(self, sensorNo):
 
-        # Read the sensor value
+        # Read the sensor values
         volts = self.read_channel(sensorNo)
-        # Based on threshold, set True or False
+        # Based on threshold, set True or False.
         inRange = volts > self.voltThresholds.get(sensorNo)
+
         # Did the value change?
         changed = False
         if self.sensorReadings[sensorNo][0] != inRange:
@@ -47,10 +46,13 @@ class Sensors():
 
         self.sensorReadings[sensorNo][0] = inRange
 
-        # Count and mark the times the value has not changed
+        # Count and mark the times the value has not changed since the last
+        # check. This is counter towards the threshold to decide if the
+        # readings are correct and should be 'determined' values.
         counter = 0
         current = self.sensorReadings[sensorNo][1]
-        if not changed and current <= self.checkThreshold: # allow to go one above
+        if not changed and current <= self.checkThreshold:
+            # Allow the checks counter go one above threshold.
             counter = current + 1
         elif not changed and current > self.checkThreshold:
             counter = current
@@ -106,57 +108,4 @@ class Sensors():
         # After checking with threshold, which are activated?
         mostRecentOpen, someChanged = self.check_changed()
 
-        if not self.monkeyInside and any(self.activatedSensors):
-            printlog('Main','Monkey came in!')
-            self.monkeyInside = True
-        elif self.monkeyInside and not any(self.activatedSensors):
-            printlog('Main','All monkeys left. :(')
-            self.monkeyInside = False
-
         return self.activatedSensors, mostRecentOpen, someChanged
-
-
-        #anyInRange = any(sensorsInRange)
-
-        # # Checking whether it is first FALSE reading after monkey has been in for interaction
-        # if (not anyInRange and self.monkeyInside):
-        #     # if monkey was inside last round and now not detected
-        #     if self.nofChecks == 0:
-        #         # First check
-        #         self.firstFalseReadingTime = datetime.now()
-        #     else:
-        #         # Not first check
-        #         self.nofChecks += 1
-        # else:
-        #     self.nofChecks = 0
-
-
-        # When enough no. of consecutive checks are same, set new value. Checks
-        # are done once per _loop_ (0.4 seconds). ScaleValue vary between 0 to
-        # threshold*factor. The set threshold is for determining if monkey
-        # inside or not (value = false or true). The more _loops_ are needed to
-        # change the userDetected value, the less sensitive the system is.
-
-        # Update value on the scale to determine monkeyInside value.
-
-        # if anyInRange:
-        #     # Sensors in range. Raise the value on scale.
-        #     if self.currentValue < (self.threshold*self.factor)-1: # don't go higher than this value.
-        #         self.currentValue += 2 # go up fast
-        # else:
-        #     # Sensors not in range. Lower the value on scale. (Not below 0). This basically will determine how long interaction is thought to last after monkey leaves (how slowly it is determined it's not inside.)
-        #     if self.currentValue > 0:
-        #         self.currentValue -= 1 # but down slower
-        #
-        # if self.currentValue > self.threshold and not self.monkeyInside:
-        #     printlog('Main','Monkey came in!')
-        #     self.monkeyInside = True
-        # elif thresholdReading < threshold and monkeyDetected:
-        #     printlog('Main','All monkeys left. :(')
-        #     self.monkeyInside = False
-        # else:
-        #     # Don't do anything yet, keep values same.
-        #     # scaleValue is now same as threshold.
-        #     pass
-        #
-        # return self.monkeyInside
